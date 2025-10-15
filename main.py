8065f6a96989c1323b3305f6916c320a8c8ecea0
dylandhw <dylanwilkins2308@gmail.com>
2025-10-14 23:56:04 -0400

    refactor

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 import cv2 
 import matplotlib.pyplot as plt 
 
-# load our image
-image = cv2.imread("examples/city.jpg")
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-plt.imshow(image_rgb)
-plt.title("Original Image")
-plt.axis("off")
-plt.show()
+def static_edge_detection(file_name):
+    # load our image
+    image = cv2.imread("examples/city.jpg")
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    plt.imshow(image_rgb)
+    plt.title("Original Image")
+    plt.axis("off")
+    plt.show()
 
-plt.imshow(gray, cmap="gray")
-plt.title("Grayscale")
-plt.axis("off")
-plt.show()
+    # convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# apply a filter 
-blur = cv2.GaussianBlur(gray, (5,5), 0)
+    plt.imshow(gray, cmap="gray")
+    plt.title("Grayscale")
+    plt.axis("off")
+    plt.show()
 
-plt.imshow(blur, cmap="gray")
-plt.title("Blurred - Reduced Noise")
-plt.axis("off")
-plt.show()
+    # apply a filter 
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+    plt.imshow(blur, cmap="gray")
+    plt.title("Blurred - Reduced Noise")
+    plt.axis("off")
+    plt.show()
 
 
-# edge detection
-edges = cv2.Canny(blur, 100, 200)
-plt.imshow(edges, cmap="gray")
-plt.title("Canny Edge Detection")
-plt.axis("off")
-plt.show()
+    # edge detection
+    edges = cv2.Canny(blur, 100, 200)
+    plt.imshow(edges, cmap="gray")
+    plt.title("Canny Edge Detection")
+    plt.axis("off")
+    plt.show()
+
+
+def main():
+    static_edge_detection("examples/city.jpg")
+    
+if __name__ == "__main__":
+    main()
