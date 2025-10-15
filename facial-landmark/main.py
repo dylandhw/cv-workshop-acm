c08a7e8ad24a9e5aab8f85faed73fca8f7596d31
dylandhw <dylanwilkins2308@gmail.com>
2025-10-15 15:09:56 -0400

    .

--- facial-landmark/main.py
+++ facial-landmark/main.py
@@ -2,9 +2,9 @@ import cv2
 import mediapipe as mp
 
 mp_drawing = mp.solutions.drawing_utils 
-mp_face_wash = mp.solutions.face_mesh 
+mp_face_mesh = mp.solutions.face_mesh 
 
-face_mesh = mp_face_wash.FaceWash(static_image_mode=False, max_num_faces=1, refine_landmarks=True)
+face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refine_landmarks=True)
 
 capture = cv2.VideoCapture(0)
 
@@ -19,4 +19,12 @@ while capture.isOpened():
 
     frame.flags.writeable = True
     
-    
+    if results.multi_face_landmakrs:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_drawing.draw_landmarks(
+                image = frame,
+                landmark_list = face_landmarks,
+                connections = mp_face_mesh.FACEMESH_TESSELATION, 
+                landmark_drawing_spec=None, 
+                connection_drawing_spec=mp_drawing.DrawingSpec(color=(0,255,0), thickness=1, circle_radius=1)
+            )
