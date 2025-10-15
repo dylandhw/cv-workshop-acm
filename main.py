a81f37204f239a3de5bdf5a922e33d36206d760f
dylandhw <dylanwilkins2308@gmail.com>
2025-10-14 23:49:02 -0400

    convert to grayscale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2 
 import matplotlib.pyplot as plt 
 
+# load our image
 image = cv2.imread("examples/city.jpg")
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
@@ -8,3 +9,13 @@ plt.imshow(image_rgb)
 plt.title("Original Image")
 plt.axis("off")
 plt.show()
+
+# convert to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+plt.imshow(gray, cmap="gray")
+plt.title("Grayscale")
+plt.axis("off")
+plt.show()
+
+
