81ffa4d219047093a2d23e2213eb22612c294832
dylandhw <dylanwilkins2308@gmail.com>
2025-10-14 23:46:01 -0400

    load image

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import cv2 
 import matplotlib.pyplot as plt 
+
+image = cv2.imread("examples/city.jpg")
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.imshow(image_rgb)
+plt.title("Original Image")
+plt.axis("off")
+plt.show()
